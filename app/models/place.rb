9ce36a97c7af9007979aa8eead11e0915acae224
admin <eric@ubuntu.(none)>
2013-02-16 11:22:32 -0800

    Added instagram initializer and vars

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,5 @@
 class Place < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :instagram_tag
   validates :user_id, presence: true
 
   belongs_to :user
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+app_variables = File.join(Rails.root, 'config', 'app_variables.rb')
+load(app_variables) if File.exists?(app_variables)
+
 # Initialize the rails application
 Wespot::Application.initialize!
