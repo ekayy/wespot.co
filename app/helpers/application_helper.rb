5710bb2e6ec9903145f807321bf1eeae56eb7a63
admin <eric@ubuntu.(none)>
2013-02-18 21:09:33 -0800

    Added coverphoto to show view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def show_user_bg
+    "background:transparent url(#{@place.coverphoto.url}) no-repeat fixed left top;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;"
+  end
 end
