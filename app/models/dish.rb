fc3f5ab8ea276329318ab950f8bf6f790d833995
Eric K Wu <ekuan.wu@gmail.com>
2013-04-05 00:34:11 -0700

    heroku to_d method error fix

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -5,10 +5,10 @@ class Dish < ActiveRecord::Base
   belongs_to :menu_category
 
   def price_in_dollars
-    price.to_d / 100 if price
+    BigDecimal(price.to_s) / 100 if price?
   end
 
   def price_in_dollars=(dollars)
-    self.price = dollars.to_d * 100 if dollars.present?
+    self.price = BigDecimal(dollars.to_s) * 100 if dollars.present?
   end
 end
